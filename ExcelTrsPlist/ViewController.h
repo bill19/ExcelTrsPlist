d6377e5aab6606ecf31b4d5bb23ac93279505687
HaoSun <sunhao.private@foxmail.com>
2016-09-29 10:40:48 +0800

    Initial Commit

---
+++ ExcelTrsPlist/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExcelTrsPlist
+//
+//  Created by HaoSun on 16/9/29.
+//  Copyright © 2016年 MaiTian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ExcelTrsPlist/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ExcelTrsPlist
+//
+//  Created by HaoSun on 16/9/29.
+//  Copyright © 2016年 MaiTian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
